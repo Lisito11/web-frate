c1c58006cc4fe9738bf7a168d3caa4676893cb1b
Lisanny Pena <l.andrespg11@gmail.com>
2023-02-22 13:07:49 -0400

    update location images

--- src/frate/components/navbar/MenuMobile.tsx
+++ src/frate/components/navbar/MenuMobile.tsx
@@ -14,7 +14,7 @@ export const MenuMobile = () => {
       <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
         <div className="flex items-center mb-8">
           <a className="mr-auto text-3xl font-bold leading-none" href="#">
-            <img src="/src/assets/logo.png" alt="" className="h-16 sm:h-20" />
+            <img src="/images/logo.png" alt="" className="h-16 sm:h-20" />
           </a>
 
           <button
